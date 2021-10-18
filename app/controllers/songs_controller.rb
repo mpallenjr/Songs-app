dc6b1641a31c91052ecb7c436e96135c6299a9e6
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-17 21:41:11 -0500

    add create route

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,10 @@
 class SongsController < ApplicationController
+  def index
+    all_songs = Song.all
+    render json: all_songs.as_json
+  end
+
+  def create
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/songs" => "songs#index"
+  post "/songs" => "songs#create"
+  
+
 end
